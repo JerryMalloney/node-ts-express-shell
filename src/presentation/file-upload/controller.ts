de2d8a7015dea0fb89198dce4f058f9f4c7d2340
Jerry Malloney <jerrymalloney@gmail.com>
2024-07-20 09:01:27 -0400

    Section 22 done

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { UploadedFile } from "express-fileupload";
+import { CustomError } from "../../domain";
+import { FileUploadService } from "../services/file-upload.service";
+
+export class FileUploadController {
+  constructor(private readonly fileUploadService: FileUploadService) {} // private readonly categoryService: CategoryService
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+    console.log(`${error}`);
+    return res.status(500).json({ error: "Internal server Error" });
+  };
+
+  uploadFile = (req: Request, res: Response) => {
+    const type = req.params.type;
+
+    const file = req.body.files[0] as UploadedFile;
+
+    this.fileUploadService
+      .uploadSingle(file, `uploads/${type}`)
+      .then((uploaded) => {
+        res.json(uploaded);
+      })
+      .catch((error) => {
+        this.handleError(error, res);
+      });
+  };
+
+  uploadMultipleFiles = (req: Request, res: Response) => {
+    const type = req.params.type;
+
+    const files = req.body.files as UploadedFile[];
+
+    this.fileUploadService
+      .uploadMultiple(files, `uploads/${type}`)
+      .then((uploaded) => {
+        res.json(uploaded);
+      })
+      .catch((error) => {
+        this.handleError(error, res);
+      });
+  };
+}
---
+++ src/presentation/file-upload/route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { FileUploadMiddleware } from "../middlewares/fie-upload.middleware";
+import { TypeMiddleware } from "../middlewares/type.middleware";
+import { FileUploadService } from "../services/file-upload.service";
+import { FileUploadController } from "./controller";
+
+export class FileUploadRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const controller = new FileUploadController(new FileUploadService());
+    router.use(FileUploadMiddleware.containFiles);
+    router.use(TypeMiddleware.validTypes(["users", "products", "categories"]));
+    // Definir las rutas
+    router.post("/single/:type", controller.uploadFile);
+    router.post("/multiple/:type", controller.uploadMultipleFiles);
+
+    return router;
+  }
+}
